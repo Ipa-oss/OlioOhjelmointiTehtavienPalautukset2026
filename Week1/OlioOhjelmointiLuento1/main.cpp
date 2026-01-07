914ec537354e8e3a149d0045bca6a03d5d18da44
Ipa-oss <backman.ilari@proton.me>
2026-01-07 11:10:05 +0200

    First commit. Changed .gitignore and added the Week 1 files from QtCreator manually.

---
+++ Week1/OlioOhjelmointiLuento1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int koe = 5;
+    printf("Hello World C:n tavalla = %d\n",koe);
+    cout << "Hello World!" << koe << endl;
+    return 0;
+}
