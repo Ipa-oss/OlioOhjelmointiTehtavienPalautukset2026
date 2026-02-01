25e8a523e2ec185526d17e56b78a14fcc880a062
Ipa-oss <backman.ilari@proton.me>
2026-02-01 19:21:49 +0200

    Fixed week 4 homework to properly calculate credit as a debt instead of a positive balance.

--- Week4/Viikkotehtava4/luottotili.cpp
+++ Week4/Viikkotehtava4/luottotili.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 Luottotili::Luottotili(string nimi,double raja):Pankkitili(nimi) {
     omistaja = nimi;
-    luottoRaja = raja;
-    saldo = luottoRaja;
+    luottoRaja = -raja;
     cout<<"Luottotili luotu "<<omistaja<<":lle, luottoraja: "<<luottoRaja<<endl;
 }
 
@@ -14,12 +13,12 @@ bool Luottotili::deposit(double in)
     if(in<=0){
         cout<<"Et voi maksaa summaa joka on nolla tai pienempi."<<endl;
         return false;
-    }else if(saldo+in>=luottoRaja){
+    }else if(saldo+in>=0){
         cout<<"Maksamasi summa on liian suuri."<<endl;
         return false;
     }else{
         saldo=saldo+in;
-        cout<<"Luottotili: talletus "<<in<<" tehty, luottoa jaljella: "<<saldo<<endl;
+        cout<<"Luottotili: talletus "<<in<<" tehty, luottoa jaljella: "<<-(luottoRaja-saldo)<<endl;
         return true;
     }
 }
@@ -29,12 +28,12 @@ bool Luottotili::withdraw(double out)
     if(out<=0){
         cout<<"Et voi nostaa summaa joka on nolla tai pienempi."<<endl;
         return false;
-    }else if(saldo-out<=0){
+    }else if(saldo-out<=luottoRaja){
         cout<<"Tilin saldo on liian pieni nostolle."<<endl;
         return false;
     }else{
         saldo=saldo-out;
-        cout<<"Luottotili: nosto "<<out<<" tehty, luottoa jaljella: "<<saldo<<endl;
+        cout<<"Luottotili: nosto "<<out<<" tehty, luottoa jaljella: "<<-(luottoRaja-saldo)<<endl;
         return true;
     }
 }
